284e0e60b81c545a5a689f2d7a27dcc3678d43bb
vikas kumar <vk8756008@gmail.com>
2025-08-06 00:51:13 +0530

    some chages

--- backend/src/index.js
+++ backend/src/index.js
@@ -35,6 +35,11 @@ if(process.env.NODE_ENV==="production"){
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   })
 } 
+
+
+
+
+
 server.listen(PORT, () => {
   console.log("server is running on port:" + PORT);
   connectDB();
