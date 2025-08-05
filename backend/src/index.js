833f98532cdc2bcf292f0aca02af1cf03ee41cae
vikas kumar <vk8756008@gmail.com>
2025-08-05 22:11:31 +0530

    bux try to fix

--- backend/src/index.js
+++ backend/src/index.js
@@ -34,8 +34,7 @@ if(process.env.NODE_ENV==="production"){
   app.get("*",(req,res)=>{
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   })
-}
-
+} 
 server.listen(PORT, () => {
   console.log("server is running on port:" + PORT);
   connectDB();
